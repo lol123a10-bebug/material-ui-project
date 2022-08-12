d571fc313c185aef10c5dc3437e8a7ffcd843338
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2022-08-12 10:01:11 +0500

    colors type;

--- src/utils/style/customMuiTheme.d.ts
+++ src/utils/style/customMuiTheme.d.ts
@@ -1,13 +1,35 @@
 declare module "@mui/material/styles" {
   interface Theme {
-    background: {
-      drawer: string;
+    colors: {
+      first: string;
+      second: string;
+      third: string;
+      textFirst: string;
+      textSecond: string;
+
+      button: string;
+
+      featured: {
+        plan: string;
+        upgrade: string;
+      };
     };
   }
 
   interface ThemeOptions {
-    status?: {
-      drawer?: string;
+    colors: {
+      first: string;
+      second: string;
+      third: string;
+      textFirst: string;
+      textSecond: string;
+
+      button: string;
+
+      featured: {
+        plan: string;
+        upgrade: string;
+      };
     };
   }
 }
